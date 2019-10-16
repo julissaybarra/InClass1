0463aeb8508cfbe036789d2e46ac8fe11a0048bd
julissaybarra <55999050+julissaybarra@users.noreply.github.com>
2019-10-16 14:43:11 -0500

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -52,6 +52,20 @@ public class StudentTest {
 	}
 	
 	@Test
+	public void testCompare() throws NameException {
+		Student firstStudent = new Student("A", "B", 2);
+		Student secondStudent = new Student("A", "C", 3);
+		Student student = new Student("A", "A", 5);
+		Student empty = new Student ("Z", "", 3);
+		Student secondEmpty = new Student("x", "", 3);
+		
+		CompareLastFirstName clfn = new CompareLastFirstName();
+		clfn.compare(firstStudent,  secondStudent);
+		
+		//////////////////finish this!!!!!!
+	}
+	
+	@Test
 	public void testGet() throws NameException {
 		Integer expected = 4;
 		Integer rating = expected;
@@ -61,5 +75,6 @@ public class StudentTest {
 		Integer actual = s.getRating();
 		assertEquals(expected, actual);
 	}
+	
 
 }
