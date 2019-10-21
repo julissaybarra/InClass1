1e6123ba4da5751519f062fd9933bf786745f8d4
julissaybarra <55999050+julissaybarra@users.noreply.github.com>
2019-10-21 14:21:28 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 //import studentName.NameException;
 //import studentName.Student;
 
@@ -17,9 +23,36 @@ public class Driver {
 			ar[3] = s2;
 			ar[4] = s3;
 			
+			NameHolder bo = new Student("O", "p", 1);
+			((Student)bo).getRating();
+			
+			
 			for(Object o: ar) {
 				System.out.println(o);
 			}
+			
+			List<Student> students = new LinkedList<>(); 
+			////can just do new ArrayList<>!!!
+			///as long as the type matches ???????? or not??
+			///i.e. ArrayList<> students = new ArrayList<>();
+			
+			students.add(s1);
+			students.add(s2);
+			students.add(s3);
+			students.add((Student) bo); //remember to typecast
+			
+			Map<String, Student> m = new HashMap<String, Student>();
+			
+			Integer index = 0;
+			for (Student s: students) {
+				m.put("s1" + index++, s); 
+				System.out.println(s);
+			}
+			
+			System.out.print(m.size());
+			
+			//List<Student> sx = new ArrayList<>(); //list is an interface, so you can't instantiate is explicitly
+			
 		} catch (NameException e) {
 			e.printStackTrace();
 		}
